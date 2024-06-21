eb4ba1db12753693e190c166299f5926c1e0d8b5
Chiel de Cock <chiel.de.cock@sogeti.com>
2024-06-21 10:27:32 +0200

    Created Spring Boot project

---
+++ src/main/java/com/sogeti/java/snake/snake_game/SnakeGameApplication.java
@@ -0,0 +1,13 @@
+package com.sogeti.java.snake.snake_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sogeti/java/snake/snake_game/SnakeGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogeti.java.snake.snake_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
